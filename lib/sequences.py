f3fc2389e5847ca1fd20d731efb44cc036f5341b
CKamande <mungagpi@gmail.com>
2025-04-13 06:56:10 +0000

    Done

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    sequence = []
+
+    if length == 0:
+        print(sequence)  # This ensures []\n is printed when length = 0
+        return
+
+    sequence.append(0)
+
+    if length == 1:
+        print(sequence)
+        return
+
+    sequence.append(1)
+
+    for _ in range(2, length):
+        next_value = sequence[-1] + sequence[-2]
+        sequence.append(next_value)
+
+    print(sequence)
+
